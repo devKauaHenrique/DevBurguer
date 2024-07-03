d14aec4d4b6e17f05823c6300540a679ae7aabcb
devKauaHenrique <kauasousa2810@gmail.com>
2024-07-02 22:57:09 -0300

    DevBurguer

--- script.js
+++ script.js
@@ -191,7 +191,7 @@ checkoutBtn.addEventListener('click', function(){
 function checkRestaurantOpen(){
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 18 && hora < 23; //true = Restaurante está aberto
+    return hora >= 18 && hora < 1; //true = Restaurante está aberto
 }
 
 const spanItem = document.getElementById('date-span')
