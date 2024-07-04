56ef85f1d4a180f44e84645183034766f91a0868
devKauaHenrique <kauasousa2810@gmail.com>
2024-07-04 19:30:49 -0300

    DevBurguer

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,12 @@ module.exports = {
     extend: {
       backgroundImage:{
         "home": "url('/assets/bg.png')"
+      },
+      keyframes:{
+        wiggle:{
+          '0%, 100%':{transform: 'translateY(0) scale(1)'},
+          '50%': {transform: 'translateY(-8px) scale(1.1)'},
+        }
       }
     },
   },
